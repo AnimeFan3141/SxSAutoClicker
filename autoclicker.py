0d5a148fbdff7c6c38e6fd1d84439e770641c0f5
Jason Darmawan <jason.darmawan@yahoo.com>
2026-05-23 13:34:21 -0500

    Fixed auto farm implementation

--- autoclicker.py
+++ autoclicker.py
@@ -2,6 +2,17 @@ import pyautogui
 import pygetwindow as gw
 import time
 
+def find_image(path, confidence=0.6):
+    try:
+        return pyautogui.locateCenterOnScreen(
+            path,
+            confidence=confidence,
+            region=region,
+            grayscale=True
+        )
+    except pyautogui.ImageNotFoundException:
+        return None
+
 # Uses OpenCV library
 # Safety feature: moving mouse to any of the 4 corners of your screen stops the program
 pyautogui.FAILSAFE = True
@@ -30,13 +41,14 @@ state4 = False #Dungeon entered to start screen
 print("Auto clicker started. Press Ctrl+C in your terminal to stop.")
 print("Looking for images...")
 
-currentDungeon = './imgs/mechasummit.png'
+## Change this variable to switch between different dungeons. Make sure to update the image in the imgs folder as well.
+currentDungeon = './imgs/Dungeons/mechasummit.png'
 
 while True:
     try:
 
         if state1:
-            img1 = pyautogui.locateCenterOnScreen(currentDungeon, confidence=0.6, region=region)
+            img1 = find_image(currentDungeon)
             if img1 is not None:
                 state1 = False
                 state2 = True
@@ -44,56 +56,64 @@ while True:
                 pyautogui.click(img1.x, img1.y)
                 time.sleep(0.5)
 
-                #tempImg1 = pyautogui.locateCenterOnScreen('./imgs/normal.png', confidence=0.6, region=region)
+                #tempImg1 = find_image('./imgs/normal.png')
                 # Click to go to normal mode
                 #pyautogui.click(tempImg1.x, tempImg1.y)
 
-                tempImg2 = pyautogui.locateCenterOnScreen('./imgs/match.png', confidence=0.6, region=region)
+                tempImg2 = find_image('./imgs/match.png')
                 # Click to start matching
                 pyautogui.click(tempImg2.x, tempImg2.y)
                 print("Matching...")
 
         if state2:
-            while True:
-                img2 = pyautogui.locateCenterOnScreen('./imgs/accept.png', confidence=0.7, region=region)
-                if img2 is not None:
-                    state2 = False
-                    state3 = True
-                    # Click to accept the matching
-                    pyautogui.doubleClick()
-                    print("Match Found")
-                    print("Loading...")
-                    break
+            img2 = find_image('./imgs/accept.png')
+            if img2 is not None:
+                state2 = False
+                state3 = True
+                # Click to accept the matching
+                pyautogui.click(img2.x, img2.y)
+                print("Match Found")
+                time.sleep(0.5)
+                print("Loading...")
 
         if state3:
-            while True:
-                entered = pyautogui.locateOnScreen('./imgs/enteredDungeon.png', confidence=0.6, region=region)
-                if entered is not None:
-                    state3 = False
-                    state4 = True
-                    print("Dungeon entered")
-                    break
-                else:
-                    tempImg = pyautogui.locateOnScreen('./imgs/queuepop.png', confidence=0.6, region=region)
-                    if tempImg is not None:
-                        state3 = False
-                        state2 = True
-                        print("Match declined, retrying...")
-                        break
+            entered = find_image('./imgs/enteredDungeon.png')
+            if entered is not None:
+                state3 = False
+                state4 = True
+                print("Dungeon entered")
+            else:
+                checkDeclined = find_image('./imgs/accept.png')
+                if checkDeclined is not None:
+                    pyautogui.click(checkDeclined.x, checkDeclined.y)
+                time.sleep(0.5)
 
         if state4:
-            img3 = pyautogui.locateOnScreen('./imgs/clearedD2.png', confidence=0.6, region=region)
-            if img3 is not None:
-                state3 = False
-                state1 = True
-                tempImg3 = pyautogui.locateCenterOnScreen('./imgs/backbutton.png', confidence=0.6, region=region)
-                # Click the center coordinates of the found image
-                pyautogui.click(tempImg3.x, tempImg3.y)
-                print("Run completed")
+            counter = 0
+            while True:
+                checkReady = find_image('./imgs/ready.png')
+                if checkReady is not None and counter < 2:
+                    pyautogui.click(checkReady.x, checkReady.y)
+                    counter = counter + 1
+                    print("Counter =", counter)
+                    time.sleep(1)
+                if counter >= 2:
+                    break
+            print("Waiting for run to complete...")
+            while True:
+                img3 = find_image('./imgs/cleared.png')
+                if img3 is not None:
+                    state4 = False
+                    state1 = True
+                    tempImg3 = find_image('./imgs/backbutton.png')
+                    # Click the center coordinates of the found image
+                    pyautogui.click(tempImg3.x, tempImg3.y)
+                    print("Run completed")
+                    time.sleep(0.1)
+                    break
+        
+        time.sleep(0.1)
 
-    except pyautogui.ImageNotFoundException:
-        # Image is not currently on screen
-        time.sleep(0.1)  # Sleep briefly to avoid high CPU usage
     except KeyboardInterrupt:
         print("Auto clicker stopped.")
         break
